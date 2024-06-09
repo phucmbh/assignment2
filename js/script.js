b15687fbd5a1ebe9b7fd8e52f91377fbfa97afb8
phucmbh <hphucdev@gmail.com>
2024-06-09 20:18:00 +0700

    update card

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-let navMenuLink = document.querySelectorAll('.nav-menu-link');
+var navMenuLink = document.querySelectorAll('.nav-menu-link');
 
-let pathname = window.location.pathname;
+var pathname = window.location.pathname;
 
 console.log(navMenuLink);
 console.log(pathname);
@@ -9,3 +9,8 @@ navMenuLink.forEach((navLink) => {
   const navLinkPathname = new URL(navLink.href).pathname;
   if (pathname === navLinkPathname) navLink.classList.add('active');
 });
+
+for (let index = 0; index < navMenuLink.length; index++) {
+  const navLinkPathname = new URL(navLink.href).pathname;
+  if (pathname === navLinkPathname) navLink.classList.add('active');
+}
